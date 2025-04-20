36d4fd77c55c35abcbb78dba24e634c6ad6cb1aa
Thomas Bjelbo <tommybjelb@live.com>
2025-04-20 22:28:36 +0200

    added pages

---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.getElementById('home').addEventListener('click', function () {
+    document.getElementById('body').classList.replace('dark', 'light');
+    document.getElementById('subTitle').classList.replace('sub-title-hide', 'sub-title-show');
+    document.getElementById('infoBox').classList.add('hide');
+    document.getElementById('signature').classList.remove('signature-corner'); 
+    setTimeout(function(){
+      document.getElementById('subTitle').classList.remove('hide');
+    }, 500)        
+});
+
+function addContentClasses() {        
+    document.getElementById('body').classList.remove('light');        
+    document.getElementById('body').classList.add('dark');
+    document.getElementById('infoBox').classList.remove('hide');
+    document.getElementById('subTitle').classList.remove('sub-title-show');      
+    document.getElementById('subTitle').classList.add('sub-title-hide');
+}
+
+function loadPage(url) {
+  addContentClasses();
+  $.get(url, function(data) {
+    var storedHTML = $(data);
+    $('#infoBox').html($(storedHTML.find("#infoBox")).html())
+    calculateExperience();
+  });
+}
+
+function calculateExperience() {
+    const startDate = new Date("2019-03-13");
+    const currentDate = new Date();
+    let yearsDiff = currentDate.getFullYear() - startDate.getFullYear();
+
+    // Adjust if the current date hasn't reached March 13 yet
+    if (
+        currentDate.getMonth() < startDate.getMonth() ||
+        (currentDate.getMonth() === startDate.getMonth() && currentDate.getDate() < startDate.getDate())
+    ) {
+        yearsDiff--;
+    }
+
+    document.getElementById("yearsExp").innerText = yearsDiff;
+}
